d9f2a9524ea60cbe6fe3b0679408abd59eb6d984
Jan Kordylewski <jkordylewski@gmail.com>
2022-03-30 13:52:16 -0500

    banachiewicz content updates

--- src/components/links/banachiewicz.js
+++ src/components/links/banachiewicz.js
@@ -16,17 +16,39 @@ const BanachiewiczLinks = () => (
     >
       Banachiewicz Wikipedia PL
     </a>
+    <a
+      target="_blank"
+      rel="noreferrer"
+      href="https://ms.polsl.pl/trysektor/materialy/Tadeusz_Banachiewicz.pdf"
+    >
+      Banachiewicz Życiorys
+    </a>
     <a target="_blank" rel="noreferrer" href="http://www.pta.edu.pl/historia">
       Polskie Towarzystwo Astronomiczne
     </a>
-    {/*<a target="_blank" rel="noreferrer" href="http://www.pta.edu.pl/orion/art/Banachiewicz/Banachiewicz2.html">An article about the Famous Astronomer</a> */}
+    <a
+      target="_blank"
+      rel="noreferrer"
+      href="https://www.pta.edu.pl/english/history"
+    >
+      Polskie Towarzystwo Astronomiczne Historia
+    </a>
+
+    <a
+      target="_blank"
+      rel="noreferrer"
+      href="http://leksykon.pta.edu.pl/index.php?id=t&kod=192"
+    >
+      Leksykon PTA
+    </a>
     <a
       target="_blank"
       rel="noreferrer"
       href="http://orion.pta.edu.pl/dlaczego-banachiewicz-na-skalce"
     >
-      Dlaczego Banachiewicz na Skałce?
+      Dlaczego Banachiewicz na Skałce? (article by Les Kordylewski)
     </a>
+    {/*<a target="_blank" rel="noreferrer" href="http://www.pta.edu.pl/orion/art/Banachiewicz/Banachiewicz2.html">An article about the Famous Astronomer</a> */}
   </div>
 );
 
--- src/pages/banachiewicz.js
+++ src/pages/banachiewicz.js
@@ -5,6 +5,7 @@ import Seo from '../components/seo';
 
 import Header from '../components/header';
 import Kordynet from '../components/kordynet';
+import BanachiewiczLinks from '../components/links/banachiewicz';
 
 const IndexPage = ({ data }) => (
   <Layout name="bana">
@@ -22,60 +23,8 @@ const IndexPage = ({ data }) => (
       </p>
       <p>The lunar crater Banachiewicz is named after him.</p>
 
-      <h3>General Links:</h3>
-      <div className="linksCopy">
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://en.wikipedia.org/wiki/Tadeusz_Banachiewicz"
-        >
-          Banachiewicz Wikipedia EN
-        </a>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://pl.wikipedia.org/wiki/Tadeusz_Banachiewicz"
-        >
-          Banachiewicz Wikipedia PL
-        </a>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="http://www.pta.edu.pl/historia"
-        >
-          Polskie Towarzystwo Astronomiczne
-        </a>
-        {/*<a target="_blank" rel="noreferrer" href="http://www.pta.edu.pl/orion/art/Banachiewicz/Banachiewicz2.html">An article about the Famous Astronomer</a> */}
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://www.pta.edu.pl/english/history"
-        >
-          Polskie Towarzystow Astronomiczne Historia
-        </a>
-
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="http://leksykon.pta.edu.pl/index.php?id=t&kod=192"
-        >
-          Leksykon PTA
-        </a>
-      </div>
-
-      <h3>My Links:</h3>
-      <div className="linksCopy">
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="http://orion.pta.edu.pl/dlaczego-banachiewicz-na-skalce"
-        >
-          Dlaczego Banachiewicz na Skałce?
-        </a>{' '}
-        (article by Les Kordylewski)
-      </div>
-
-      <div className="clearBoth"></div>
+      <h3>Links:</h3>
+      <BanachiewiczLinks />
       <Kordynet />
     </div>
   </Layout>
