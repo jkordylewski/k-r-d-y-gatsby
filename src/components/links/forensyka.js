51df35c6a2dffddcd53273e709e0f2fdc2d89018
Jan Kordylewski <jkordylewski@gmail.com>
2022-03-30 13:12:56 -0500

    new link component for forensyka

---
+++ src/components/links/forensyka.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const ForensykaLinks = () => (
+  <div class="linksCopy">
+    <a target="_blank" rel="noreferrer" href="http://www.smsi.org/">
+      State Microscopical Society of Illinois
+    </a>
+    <a target="_blank" rel="noreferrer" href="http://www.aafs.org/">
+      The American Academy of Forensic Sciences
+    </a>
+    <a target="_blank" rel="noreferrer" href="http://www.mafs.net/">
+      Midwestern Association of Forensic Scientists
+    </a>
+    <a target="_blank" rel="noreferrer" href="http://www.forensicdna.com/">
+      Forensic DNA
+    </a>
+  </div>
+);
+
+export default ForensykaLinks;
