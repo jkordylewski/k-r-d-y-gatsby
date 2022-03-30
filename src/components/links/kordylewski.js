9b0787ed41fdf343fbd5d4e0b8660835af6198ed
Jan Kordylewski <jkordylewski@gmail.com>
2022-03-30 13:00:47 -0500

    new links component

---
+++ src/components/links/kordylewski.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const KazimierzLinks = () => (
+  <div className="linksCopy">
+    <a
+      target="_blank"
+      rel="noreferrer"
+      href="https://en.wikipedia.org/wiki/Kazimierz_Kordylewski"
+    >
+      Kazimierz Kordylewski Wikipedia EN
+    </a>
+    <a
+      target="_blank"
+      rel="noreferrer"
+      href="https://en.wikipedia.org/wiki/Kordylewski_cloud"
+    >
+      Kordylewski Cloud Wikipedia EN
+    </a>
+    <a
+      target="_blank"
+      rel="noreferrer"
+      href="https://pl.wikipedia.org/wiki/Kazimierz_Kordylewski"
+    >
+      Kazimierz Kordylewski Wikipedia PL
+    </a>
+    <a
+      target="_blank"
+      rel="noreferrer"
+      href="https://pl.wikipedia.org/wiki/Ksi%C4%99%C5%BCyce_Kordylewskiego"
+    >
+      Księżyce Kordylewskiego (Kordylewski Cloud) Wikipedia PL
+    </a>
+    <a target="_blank" rel="noreferrer" href=" http://www.kordylewski.pl/">
+      Kordylewski.pl
+    </a>
+    <a
+      target="_blank"
+      rel="noreferrer"
+      href="  https://radiopolska.pl/90lat/powojenna-odbudowa-polskiej-radiofonii/leszek-kordylewski-o-przygodach-przy-nadawaniu-sygnalu-czasu"
+    >
+      RadioPolska Interview - Leszek Kordylewski o przygodach przy nadawaniu
+      sygnału czasu
+    </a>
+  </div>
+);
+
+export default KazimierzLinks;
